56b8615661fea385222d926ce0f04033d6fcc5fb
Jake <jakeroggenbuck2@gmail.com>
2024-10-05 14:13:19 -0700

    Add game loop and enum functions

--- main.go
+++ main.go
@@ -10,6 +10,26 @@ const (
 	WHITE
 )
 
+func (p Piece) Name() string {
+	strings := [...]string{"EMPTY", "Black", "White"}
+
+	if p < EMPTY || p > WHITE {
+		return "Unknown"
+	}
+
+	return strings[p]
+}
+
+func (p Piece) String() string {
+	strings := [...]string{"+", "B", "W"}
+
+	if p < EMPTY || p > WHITE {
+		return "?"
+	}
+
+	return strings[p]
+}
+
 type Board struct {
 	Board [][]Piece
 	Size  int
@@ -28,12 +48,27 @@ func (b Board) CanPlace(i int, j int) bool {
 }
 
 func (b Board) Unset(i int, j int) {
-	b.Board[i][j] = EMPTY;
+	b.Board[i][j] = EMPTY
 }
 
 func (b Board) Display() {
+	fmt.Print("    ")
 	for i := 0; i < b.Size; i++ {
-		fmt.Println(b.Board[i])
+		val := (i % 10 + 1)
+		if val > 9 {
+			val = 0
+		}
+		fmt.Print(val, " ")
+	}
+	fmt.Println()
+	for i := 0; i < b.Size; i++ {
+		padding := ""
+		// not < 10 because index + 1 for display
+		if i < 9 {
+			padding = " "
+		}
+
+		fmt.Printf("%s%d %v\n", padding, i+1, b.Board[i])
 	}
 }
 
@@ -57,10 +92,28 @@ func main() {
 
 	fmt.Println("Go Game in Go Lang")
 
-	board.Display()
+	current_player := 1
 
-	board.Set(0, 0, BLACK)
-	fmt.Println("Set (0, 0) to BLACK")
+	// Game Loop
+	for {
+		fmt.Printf("Play %d's turn to place a %v piece\n", current_player, Piece(current_player).Name())
 
-	board.Display()
+		var x int
+		var y int
+
+		board.Display()
+
+		fmt.Print("Type in coordinates (e.g. 2, 3): ")
+		fmt.Scan(&x)
+		fmt.Scan(&y)
+
+		// Note the swap of x and y for i and j and the 1 indexed input coords
+		board.Set(y-1, x-1, Piece(current_player))
+
+		if current_player == 1 {
+			current_player = 2
+		} else {
+			current_player = 1
+		}
+	}
 }
