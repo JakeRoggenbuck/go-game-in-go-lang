29de253dd9291dd998e628b5879d3b3368f67aca
Jake <jakeroggenbuck2@gmail.com>
2024-10-05 13:18:31 -0700

    Add piece

--- main.go
+++ main.go
@@ -2,20 +2,30 @@ package main
 
 import "fmt"
 
-type board struct {
-	board [][]int
+type Piece int
+
+const (
+	EMPTY Piece = iota
+	BLACk
+	WHITE
+)
+
+type Board struct {
+	Board [][]Piece
 }
 
-func set ()
+func (b Board) set(i int, j int, p Piece) {
+	b.Board[i][j] = p
+}
 
 func main() {
 	board_size := 10
 
 	fmt.Println("Go Game in Go Lang")
 
-	board := make([][]int, board_size)
+	board := Board{Board: make([][]Piece, board_size)}
 	for i := 0; i < board_size; i++ {
-		board[i] = make([]int, board_size)
-		fmt.Println(board[i])
+		board.Board[i] = make([]Piece, board_size)
+		fmt.Println(board.Board[i])
 	}
 }
