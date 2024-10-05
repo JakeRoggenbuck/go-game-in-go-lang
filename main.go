46792b30e425780da65d9915ae7931a6dbd9ad62
Jake <jakeroggenbuck2@gmail.com>
2024-10-05 13:23:49 -0700

    Add Set and Display

--- main.go
+++ main.go
@@ -6,16 +6,23 @@ type Piece int
 
 const (
 	EMPTY Piece = iota
-	BLACk
+	BLACK
 	WHITE
 )
 
 type Board struct {
 	Board [][]Piece
+	Size  int
 }
 
-func (b Board) set(i int, j int, p Piece) {
+func (b Board) Set(i int, j int, p Piece) {
 	b.Board[i][j] = p
+}
+
+func (b Board) Display() {
+	for i := 0; i < b.Size; i++ {
+		fmt.Println(b.Board[i])
+	}
 }
 
 func main() {
@@ -23,9 +30,19 @@ func main() {
 
 	fmt.Println("Go Game in Go Lang")
 
-	board := Board{Board: make([][]Piece, board_size)}
+	board := Board{
+		Board: make([][]Piece, board_size),
+		Size:  board_size,
+	}
+
 	for i := 0; i < board_size; i++ {
 		board.Board[i] = make([]Piece, board_size)
-		fmt.Println(board.Board[i])
 	}
+
+	board.Display()
+
+	board.Set(0, 0, BLACK)
+	fmt.Println("Set (0, 0) to BLACK")
+
+	board.Display()
 }
